aa88fa737e8ffe8c251638e4511dcbcbb669b396
HenonoaH <heno@founderandlightning.com>
2024-02-12 14:47:24 +0530

    fixed undefined environment variables

--- src/config.js
+++ src/config.js
@@ -4,15 +4,11 @@ const config = () => {
       featureToggles: {
         UIRefresh2022: true,
       },
-      port: process.env.PORT,
-      host: process.env.HOST,
     },
     development: {
       featureToggles: {
         UIRefresh2022: true,
       },
-      port: process.env.PORT || '3000',
-      host: process.env.HOST || 'localhost',
     },
   }
   return process.env.ENVIRONMENT ? env[process.env.ENVIRONMENT] : env
